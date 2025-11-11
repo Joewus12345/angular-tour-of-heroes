026a80b53e723d3488d6b7cb9b9a4f246cb70b1e
Joewus12345 <joewus12345@gmail.com>
2025-11-11 18:08:08 +0000

    toh-pt5 complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,32 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 import { CommonModule, NgFor } from '@angular/common';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { AppRoutingModule } from "../app-routing.module";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, NgFor, HeroDetailComponent],
+  imports: [CommonModule, NgFor, AppRoutingModule],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
-
-  selectedHero?: Hero;
-  
+export class HeroesComponent implements OnInit {  
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
